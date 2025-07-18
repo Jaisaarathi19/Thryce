8350aa1f72679a28bc333b75039bc071e975497e
Jaisaarathi19 <221801019@rajalakshmi.edu.in>
2025-07-19 00:14:04 +0530

    chumma

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -75,7 +75,7 @@ const ServicesPage: React.FC = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Services data
+  // Services dataS
   const services = [
     {
       title: "Web Development",
@@ -93,7 +93,7 @@ const ServicesPage: React.FC = () => {
       title: "Logo Design",
       description: "Designing memorable and impactful logos that capture your brand's essence and make a lasting impression.",
       icon: "fa-pen-nib",
-      image: "/logo/THRYCE_black_logo_full.jpg"
+      image: "/logo/THRYCE_white_logo_full.jpg"
     }
     // Add more services as needed
   ];
